3b318c3c397d9aafdcb1a316bba05257f79aedfc
Mitchell Rodríguez <mitrodle@gmail.com>
2021-10-15 23:14:01 -0500

    add: ngFor + interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
-  items = ['😀','😁','😂','🤣']
+
+  products : Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  items = ['😀','😁','😂','🤣'];
 
   addItem() {
-    this.items.push(this.title)
+    this.items.push(this.title);
   }
 
-  removeItem(i : number) {
-    this.items.splice(i, 1)
+  removeItem(index : number) {
+    this.items.splice(index, 1);
   }
+
 }
